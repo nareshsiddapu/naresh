b71f908067518e414e757d8030db5144ebddeb83
siddapunaresh <siddapunaresh@gmail.com>
2020-10-05 16:35:36 +0530

    first commit

---
+++ 1.c
@@ -0,0 +1,13 @@
+//integer arthematic operations//
+
+#include<stdio.h>
+int main()
+{
+	int a=17 , b=4;
+	printf("sum=%d\n",a+b);
+	printf("difference=%d\n",a-b);
+	printf("product=%d\n",a*b);
+	printf("quotient=%d\n",a/b);
+	printf("remainder=%d\n",a%b);
+	return 0;
+}
---
+++ 3.c
@@ -0,0 +1,14 @@
+/*postfix increment/decrement */
+
+#include<stdio.h>
+int main()
+{
+        int x=8 , y=5 ;
+        printf("x=%d\t",x);
+        printf("x=%d\t",x--);
+        printf("x=%d\t",x);
+        printf("x=%d\t",y++);
+        printf("x=%d\t",y);
+        return 0;
+}
+
